58d5e14331ea00c8db93463cd7fe6d6d1f028655
Jennifer <jennifer@Panda.(none)>
2014-04-16 21:32:16 -0300

    codigo finalizado do menu

--- Codigos/menu.h
+++ Codigos/menu.h
@@ -34,6 +34,47 @@ void carregarTela()
         al_destroy_display(Display);
     }
 
-    
+    if(!iniciarFogo)
+    {  
+        fogoMenu = al_load_bitmap(Imagens//*Primeira imagem do tipo Fogo*/);
+        fprintf(stderr,"Falha ao carregar Fogo1\n");
+    }
+    else
+    {
+        fogoMenu = al_load_bitmap(Imagens//*Segunda imagem do tipo Fogo*/);
+        fprintf(stderr, "Falha ao carregar Fogo2\n");
+    }
+
+    if(!iniciarAgua)
+    {
+        aguaMenu = al_load_bitmap(Imagens//*Primeira imagem do tipo Agua*/);
+        fprintf(stderr, "Falha ao carregar Agua1\n");
+    }
+    else
+    {
+        aguaMenu = al_load_bitmap(Imagens//*Segunda imagem do tipo Agua*/)
+        fprintf(stderr, "Falha ao carregar Agua2\n");
+    }
+
+    if(!iniciarPlanta)
+    {
+        plantaMenu = al_load_bitmap(Imagens//*Primeira imagem de Planta*/);
+        fprintf(stderr, "Falha ao carregar Planta1\n");
+    }
+
+    else
+        {
+            plantaMenu = al_load_bitmap(Imagens//*Segunda imagem de Planta*/);
+            fprintf(stderr, "Falha ao carregar Planta2\n");
+        }
+
+    al_set_target_bitmap(al_get_backbuffer(Display));
+    al_draw_bitmap(fundoDisplay,0,0,0);
+    al_draw_bitmap(iniciarMenu,300 ,170 , 0);
+    al_draw_bitmap(continuarMenu, 290,250,0);
+    al_draw_bitmap(sairMenu,360,320,0);
+    // Atualiza a tela
+    al_flip_display();
+   
 
 }
