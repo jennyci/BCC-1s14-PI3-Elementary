fa5ea61c613c1fa9c3156883b08d389b266f6e67
Raldney Alves <raldney.alves@gmail.com>
2014-06-08 13:34:05 -0300

    preparando inserção de imagens

--- Codigos/src/elementary.c
+++ Codigos/src/elementary.c
@@ -3,12 +3,9 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
-
 #include "../libs/camera.h"
-
-
-
 #define FPS 60
+
 void RGB2HSV(int red, int green, int blue, int *h, int *s, int *v){
   float r = (float)red/255;
   float g = (float)green/255;
@@ -71,9 +68,9 @@ void RGB2HSV(int red, int green, int blue, int *h, int *s, int *v){
   
     *v = cMax * 100;
 }
-void cameraRastreia(unsigned char ***matriz, camera *cam,  ALLEGRO_BITMAP *direita){
-  camera_atualiza(cam);
+void cameraRastreia(unsigned char ***matriz, camera *cam,  ALLEGRO_BITMAP *direita,int *px, int *py){
 
+  camera_atualiza(cam);
     int x, y;
     int cyr = 0;
     int cxr = 0;
@@ -85,7 +82,7 @@ void cameraRastreia(unsigned char ***matriz, camera *cam,  ALLEGRO_BITMAP *direi
         for(x = 0; x < cam->largura; x++){
             RGB2HSV(cam->quadro[y][x][0], cam->quadro[y][x][1], cam->quadro[y][x][2], &h, &s, &v);
 
-            if(h < 25 || h > 345)
+            if(h < 10 || h > 345)
               if(s > 75 && v > 75){
                   cyr += y;
                   cxr += x;
@@ -95,29 +92,21 @@ void cameraRastreia(unsigned char ***matriz, camera *cam,  ALLEGRO_BITMAP *direi
     }
 
     /**********/
-    camera_copia(cam, cam->quadro, direita);
-
-
-    if(cnr > 20){
+  
+    if(cnr > 15){
         x = cxr / cnr;
         y = cyr / cnr;
         al_draw_circle(x, y, 50, al_map_rgb(0, 0, 255),2);
         al_draw_filled_circle(x, y, 5, al_map_rgb(0, 0, 255));
-
     }
-
-  
+    *px = x;
+    *py = y;
 }
-
-
 void erro(char *mensagem) {
   fputs(mensagem, stderr);
 
   exit(EXIT_FAILURE);
 }
-
-
-
 void mediana(camera *cam){
     int x, y;
     for(y = 0; y < cam->altura; y++){
@@ -159,12 +148,11 @@ void mediana(camera *cam){
 	 }
 }
 
-
 int main() {
   camera *cam = camera_inicializa(0);
   if(!cam)
     erro("erro na inicializacao da camera\n");
-
+  int x, y;
   int largura = cam->largura;
   int altura = cam->altura;
 
@@ -181,7 +169,7 @@ int main() {
   if(!timer)
     erro("erro na criacao do relogio\n");
 
-  ALLEGRO_DISPLAY *display = al_create_display(2 * largura, altura);
+  ALLEGRO_DISPLAY *display = al_create_display(2 * largura,altura);
   if(!display)
     erro("erro na criacao da janela\n");
 
@@ -202,15 +190,22 @@ int main() {
   ALLEGRO_COLOR cor = al_map_rgb_f(0, 255, 0);
 
   ALLEGRO_BITMAP *buffer = al_get_backbuffer(display);
+  ALLEGRO_BITMAP *fundo = al_load_bitmap("Imagens/Elementary2.png");
+  //ALLEGRO_BITMAP *escolha = al_load_bitmap("Imagens/Elementary.png");
+  if(!fundo)
+    erro("erro ao carregar Elementary.png");
 
   ALLEGRO_BITMAP *esquerda = al_create_sub_bitmap(buffer, 0, 0, largura, altura);
-
   ALLEGRO_BITMAP *direita = al_create_sub_bitmap(buffer, largura, 0, largura, altura);
 
   /**********/
 
   int desenhar = 0;
   int terminar = 0;
+  int count = 0;
+  al_set_target_bitmap(esquerda);
+         al_draw_bitmap(fundo,0,0,0);
+         al_draw_filled_circle(120,380,80,al_map_rgb(255,0,255));
   while(1) {
     ALLEGRO_EVENT event;
 
@@ -223,8 +218,7 @@ int main() {
     case ALLEGRO_EVENT_DISPLAY_CLOSE:
       terminar = 1;
       break;
-    default:
-      printf("evento desconhecido\n");
+   
     }
 
     if(terminar)
@@ -235,18 +229,30 @@ int main() {
       camera_atualiza(cam);
       mediana(cam);
       /**********/
-cameraRastreia(matriz,cam,esquerda);//ONDE FICARÁ O ALLEGRO
-camera_copia(cam,cam->quadro,direita);
+        al_set_target_bitmap(esquerda);
+         al_draw_bitmap(fundo,0,0,0);
+        cameraRastreia(matriz,cam,esquerda,&x,&y);//ONDE FICARÁ O ALLEGRO
+        if(x >=40 && x<=200 )
+          if(y <=460 && y>=360 ){
+            count++;
+          }
+        if(count >=10)
+            fundo = al_load_bitmap("Imagens/Fundo.jpg");
             /**********/
             /**********/
+              
+            al_set_target_bitmap(direita);
+    camera_copia(cam, cam->quadro, direita);
             al_flip_display();
+
+
         }
     }
 
   /**********/
 
   al_destroy_bitmap(direita);
-
+  al_destroy_bitmap(fundo);
   al_destroy_bitmap(esquerda);
 
   camera_libera_matriz(cam, matriz);
