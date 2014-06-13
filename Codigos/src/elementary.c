980b45a1fe624cec0ccd4dc7a8bf839c35c0d123
Raldney Alves <raldney.alves@gmail.com>
2014-06-12 22:52:51 -0300

    JOGO FINALIZADO

--- Codigos/src/elementary.c
+++ Codigos/src/elementary.c
@@ -2,18 +2,18 @@
 #define FPS 60
 
 typedef struct {
-  int pos_x,pos_y;
+  int pos_x,pos_y,tipo;
   ALLEGRO_BITMAP *elemento;
   bool status;
 } Disco;
 
-bool abrirJogo(int x, int y,int *fps,int *tempo,ALLEGRO_FONT *font,ALLEGRO_COLOR font_color){
+bool abrirJogo(int x, int y,int *fps,int *tempo,ALLEGRO_FONT *font,ALLEGRO_FONT *font2,ALLEGRO_COLOR font_color){
   char seg[100];
   
   if(x >=40 && x<=200 && y <=460 && y>=360){
               *fps = *fps+1;
               sprintf(seg, "%d", *tempo); 
-              al_draw_text(font, font_color, 120, 200, 0,seg);
+              al_draw_text(font2, font_color, 120, 200, 0,seg);
               
 
               if(*fps >= 3){
@@ -41,46 +41,63 @@ void carregarDiscos(Disco **discos){
   discos[1]->pos_x = rand()%9 * 55;
   discos[1]->pos_y = 0;
   discos[1]->status = false;
+  discos[1]->tipo = 1;
+
   discos[2] = malloc(sizeof(Disco));
   discos[2]->elemento = al_load_bitmap("Imagens/fogo.png");
   discos[2]->pos_x = rand()%9 * 55;
   discos[2]->pos_y = 0;
   discos[2]->status = false;
+  discos[2]->tipo = 2;
+
   discos[3] = malloc(sizeof(Disco));
   discos[3]->elemento = al_load_bitmap("Imagens/planta.png");
   discos[3]->pos_x = rand()%9 * 55;
   discos[3]->pos_y = 0;
   discos[3]->status = false;
+  discos[3]->tipo = 3;
+
   discos[4] = malloc(sizeof(Disco));
   discos[4]->elemento = al_load_bitmap("Imagens/agua.png");
   discos[4]->pos_x = rand()%9 * 55;
   discos[4]->pos_y = 0;
   discos[4]->status = false;
+  discos[4]->tipo = 1;
+
   discos[5] = malloc(sizeof(Disco));
   discos[5]->elemento = al_load_bitmap("Imagens/fogo.png");
   discos[5]->pos_x = rand()%9 * 55;
   discos[5]->pos_y = 0;
   discos[5]->status = false;
+  discos[5]->tipo = 2;
+
   discos[6] = malloc(sizeof(Disco));
   discos[6]->elemento = al_load_bitmap("Imagens/planta.png");
   discos[6]->pos_x = rand()%9 * 55;
   discos[6]->pos_y = 0;
   discos[6]->status = false;
+  discos[6]->tipo = 3;
+
   discos[7] = malloc(sizeof(Disco));
   discos[7]->elemento = al_load_bitmap("Imagens/agua.png");
   discos[7]->pos_x = rand()%9 * 55;
   discos[7]->pos_y = 0;
   discos[7]->status = false;
+  discos[7]->tipo = 1;
+
   discos[8] = malloc(sizeof(Disco));
   discos[8]->elemento = al_load_bitmap("Imagens/fogo.png");
   discos[8]->pos_x = rand()%9 * 55;
   discos[8]->pos_y = 0;
   discos[8]->status = false;
+  discos[8]->tipo = 2;
+
   discos[9] = malloc(sizeof(Disco));
   discos[9]->elemento = al_load_bitmap("Imagens/planta.png");
   discos[9]->pos_x = rand()%9 * 55;
   discos[9]->pos_y = 0;
   discos[9]->status = false;
+  discos[9]->tipo = 3;
 }
 void erro(char *mensagem) {
   fputs(mensagem, stderr);
@@ -90,9 +107,9 @@ void erro(char *mensagem) {
 
 int main() {
   bool menu = true;
-
+  char pontuacao[100];
   ALLEGRO_COLOR  font_color;
-  ALLEGRO_FONT *font;
+  ALLEGRO_FONT *font,*font2;
   camera *cam = camera_inicializa(0);
   if(!cam)
     erro("erro na inicializacao da camera\n");
@@ -114,6 +131,9 @@ int main() {
     font_color = al_map_rgb(0, 0, 0);
     
     font = al_load_ttf_font("Fontes/Blokletters-Viltstift.ttf", 20, 0);
+    font2 = al_load_ttf_font("Fontes/Blokletters-Viltstift.ttf", 40, 0);
+    
+
 
   if(!al_init_primitives_addon())
     erro("erro na inicializacao do adicional de primitivas\n");
@@ -147,8 +167,10 @@ int main() {
 
   /**********/
   Disco *discos[9];
+  bool perdeu = false;
   carregarDiscos(discos);
-
+  int pontos=0,velo = 1;
+  int aux1 = 1;
   int ultimoDisco = 0;
   int distance = 0;
   int desenhar = 0;
@@ -183,38 +205,78 @@ int main() {
       al_draw_bitmap(fundo,0,0,0);
       
       if(!menu){
-        for(int aux1 = 1; aux1 <= ndisco;aux1++){
-          if((!discos[aux1]->status && distance > 200) || ultimoDisco==0 ){
-            printf("%d\n",aux1 );
-            al_draw_bitmap(discos[aux1]->elemento,discos[aux1]->pos_x,discos[aux1]->pos_y,0);
-            discos[aux1]->status = true;
-            distance = 0;
-            ultimoDisco = aux1;
-            printf("Ultimo disco: %d\n",aux1 );
-            break;
+        while(aux1 <= ndisco){
+          if(discos[aux1]->status == false){
+            if(distance > 50 || ultimoDisco==0  ){
+              printf("%d\n",aux1 );
+              al_draw_bitmap(discos[aux1]->elemento,discos[aux1]->pos_x,discos[aux1]->pos_y,0);
+              discos[aux1]->status = true;
+              distance = 0;
+              ultimoDisco = aux1;
+              break;
+            }else
+             aux1++;
           }else{
-            discos[aux1]->pos_y+=10;
-            al_draw_bitmap(discos[aux1]->elemento,discos[aux1]->pos_x,discos[aux1]->pos_y,0);
-          }
-            distance = discos[ultimoDisco]->pos_y;
-          /*if(discos[1]->pos_y > 480){
-            al_draw_text(font, al_map_rgb(255, 0, 0), 200, 200, 0,"VOCÊ PERDEU!!!");
-            al_flip_display();
-            al_rest(5);
-            break;
-          }
-      al_draw_text(font, al_map_rgb(255, 255, 255), 240, 5, 0,"PONTUAÇÃO");*/
+              discos[aux1]->pos_y+=(10+velo);
+              al_draw_bitmap(discos[aux1]->elemento,discos[aux1]->pos_x,discos[aux1]->pos_y,0);
+            aux1++;
+              
+            }
         }
+        distance = discos[ultimoDisco]->pos_y;
+        for(aux1 = 1;aux1<ndisco;aux1++){
+          if(discos[aux1]->pos_x >= x-30 && discos[aux1]->pos_x <= x+30 && discos[aux1]->pos_y >= y-30 &&  discos[aux1]->pos_y <= y+30){
+            if(discos[aux1]->tipo == 2){ // Tipo do fogo(Necessario para vencer o jogo)
+            pontos +=10;
+            velo += 2;
+            discos[aux1]->pos_x = rand()%9 * 55;
+            discos[aux1]->pos_y = 0;
+            discos[aux1]->status = false;
+          }else if(discos[aux1]->tipo == 1){ //Tipo da agua(Perde o jogo se destruir esse disco)
+            al_draw_text(font2, al_map_rgb(255, 0, 0), 70, 200, 0,"VOCÊ PERDEU!!!");
+            sprintf(pontuacao,"PONTUAÇÃO FINAL: %d",pontos);
+            al_draw_text(font2, al_map_rgb(255, 255, 255), 50, 260, 0,pontuacao);
+            al_flip_display();
+            al_rest(3);
+            perdeu = true;
+          }else if(discos[aux1]->tipo == 3){//Tipo planta(Aumenta velocidade de queda das peças)
+            velo *= 2;
+            discos[aux1]->pos_x = rand()%9 * 55;
+            discos[aux1]->pos_y = 0;
+            discos[aux1]->status = false;
+          }
+
+        }else if( discos[aux1]->pos_y > 480){
+          if(discos[aux1]->tipo == 2){ //Tipo da agua e Planta(Não perde se deixar cair)
+            al_draw_text(font2, al_map_rgb(255, 0, 0), 70, 200, 0,"VOCÊ PERDEU!!!");
+            sprintf(pontuacao,"PONTUAÇÃO FINAL: %d",pontos);
+            al_draw_text(font2, al_map_rgb(255, 255, 255), 50,260, 0,pontuacao);
+            al_flip_display();
+            al_rest(2);
+            perdeu = true;
+          }else{
+            discos[aux1]->pos_x = rand()%9 * 55;
+            discos[aux1]->pos_y = 0;
+            discos[aux1]->status = false;
+          }
+        }
+      }
+
+        aux1 = 1;
+      sprintf(pontuacao,"PONTUAÇÃO: %d",pontos);
+      al_draw_text(font, al_map_rgb(255, 255, 255), 50, 5, 0,pontuacao);
 
       }
+      if(perdeu)
+        break;
       if(menu){
-        if(abrirJogo(x,y,&fps,&tempo,font, font_color)){
+        if(abrirJogo(x,y,&fps,&tempo,font,font2, font_color)){
           fundo = al_load_bitmap("Imagens/galaxia.png");
           menu = false;
         }
       }
       cameraRastreia(cam,&x,&y);
-      
+   
 
       al_set_target_bitmap(direita);
       camera_copia(cam, cam->quadro, direita);
